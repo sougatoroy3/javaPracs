49bc12f71513bc60dbc561835a053ea46b8cb01f
Sougato Roy <sougato.221020010@muj.manipal.edu>
2023-12-28 10:49:06 +0530

    pattern3 update

--- pattern3.java
+++ pattern3.java
@@ -5,12 +5,19 @@ public class pattern3 {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        for(int i=1; i<=n; i++)
+        //for(int i=1; i<=n; i++)
+        //{
+        //    for(int j=1; j<=n-i+1; j++)
+        //    {
+        //        System.out.print(j+" ");
+        //    }
+        //    System.out.println();
+        //}
+
+        for(int i=n; i>=1; i--)
         {
-            for(int j=1; j<=n-i+1; j++)
-            {
-                System.out.print(j+" ");
-            }
+            for(int j=1; j<=i; j++)
+                System.out.print(j);
             System.out.println();
         }
     }
